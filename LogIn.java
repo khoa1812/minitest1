efba7c625ee0664bb9b6ff632b292d94d9ee72dd
khoa1812 <khoatrinh2909@gmail.com>
2023-12-12 09:01:43 +0700

    2

--- LogIn.java
+++ LogIn.java
@@ -2,76 +2,76 @@ package MiniTest;
 
 import java.util.Scanner;
 
-public class DangNhap {
+public class LogIn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String user, password;
-        int soLanThu = 0;
+        int number = 0;
 
-        while (soLanThu < 3) {
+        while (number < 3) {
             System.out.print("Nhap user: ");
             user = scanner.nextLine();
             System.out.print("Nhap password: ");
             password = scanner.nextLine();
 
             if (user.equals("admin") && password.equals("123456")) {
-                HienThiMenu();
+                Display();
                 break;
             } else {
                 System.out.println("User hoac password khong dung. Vui long nhap lai.");
-                soLanThu++;
+                number++;
             }
         }
 
-        if (soLanThu == 3) {
+        if (number == 3) {
             System.out.println("Ban da nhap sai qua 3 lan. Chương trình bị khóa.");
         }
     }
 
 
-    public static void HienThiMenu() {
+    public static void Display() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Menu:");
         System.out.println("1. Ve hinh chu nhat");
         System.out.println("2. Ve tam giac vuong");
         System.out.println("3. Ve tam giac can");
         System.out.print("Lua chon cua ban: ");
-        int luaChon = scanner.nextInt();
+        int choice = scanner.nextInt();
 
-        switch (luaChon) {
+        switch (choice) {
             case 1:
                 System.out.print("Nhap chieu rong: ");
-                int rong = scanner.nextInt();
+                int width = scanner.nextInt();
                 System.out.print("Nhap chieu cao: ");
-                int cao = scanner.nextInt();
-                veHinhChuNhat(rong, cao);
+                int hight = scanner.nextInt();
+                rectangle(width, hight);
                 break;
             case 2:
                 System.out.print("Nhap chieu cao cua tam giac vuong: ");
-                int caoTG = scanner.nextInt();
-                veTamGiacVuong(caoTG);
+                int hightrT = scanner.nextInt();
+                rightTriangle(hightrT);
                 break;
             case 3:
                 System.out.print("Nhap chieu cao cua tam giac can: ");
-                int caoTGC = scanner.nextInt();
-                veTamGiacCan(caoTGC);
+                int hightiT = scanner.nextInt();
+                isoscelesTriangle(hightiT);
                 break;
             default:
                 System.out.println("Lua chon khong hop le");
         }
     }
 
-    public static void veHinhChuNhat(int rong, int cao) {
-        for (int i = 0; i < cao; i++) {
-            for (int j = 0; j < rong; j++) {
+    public static void rectangle(int width, int hight) {
+        for (int i = 0; i < hight; i++) {
+            for (int j = 0; j < width; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 
-    public static void veTamGiacVuong(int cao) {
-        for (int i = 0; i < cao; i++) {
+    public static void rightTriangle(int hight) {
+        for (int i = 0; i < hight; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
@@ -79,9 +79,9 @@ public class DangNhap {
         }
     }
 
-    public static void veTamGiacCan(int cao) {
-        for (int i = 0; i < cao; i++) {
-            for (int j = 0; j < cao - i - 1; j++) {
+    public static void isoscelesTriangle(int hight) {
+        for (int i = 0; i < hight; i++) {
+            for (int j = 0; j < hight - i - 1; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
